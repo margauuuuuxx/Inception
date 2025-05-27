74f72e11a2fb16c11ac542e10c70ffd288c13b74
marlonco <marlonco@inception.myguest.virtualbox.org>
2025-05-27 14:46:48 +0200

    trop de doc tue la doc

---
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# exit immediatly if a command exits with a non-0 status
+set -e
+
+# if the data directiry doesnt exist create it
+if [! -d /var/lib/mysql/mysql ]; then 
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql #initializes the dir
+fi
+
+exec mysqld_safe #starts the database with an extra security layer (restarts the serber in case of error)
---
+++ srcs/requirements/nginx/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
+    # -x509                 --> generates a certificate instead of a certificate request
+    # -nodes                --> do not encryot the private key because needed by nginx
+    # -days 365             --> valid for 1 year
+    # -subj "/CN=localhost" --> sets certificate subject: CN is the Common Name (domain name)
+    # -newkey rsa:2048      --> creates a new 2048-bits (min recommended key size) RSA key pair
+    # -keyout <>            --> path to save the private key
+    # -out <>               --> path to save the certificate
+    open ssl req -x509 -nodes -days 365 \
+    -subj "/CN=localhost" \
+    -newkey rsa:2048 \
+    -keyout /etc/ssl/private/nginx-selfsigned.key \
+    -out /etc/ssl/certs/nginx-selfsigned.crt
+fi
+
+# -g 'daemon off;' --> overrides nginx default config to make sure that it runs in the foreground (and not background)
+# important bc otherwise it'll be daemonized (= go to the background) and the containers will exit immediately
+exec nginx -g 'daemon off;'
