d181aa35eea5e1ec7e5df522ac8601eda1c6922b
marlonco <marlonco@inception.myguest.virtualbox.org>
2025-05-29 12:29:01 +0200

    nginx certificate and key

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -2,22 +2,31 @@
 
 set -e
 
+DOMAIN="${DOMAIN_NAME:-localhost}"
+
+CRT="/etc/ssl/certs/${DOMAIN_NAME}.crt"
+KEY="/etc/ssl/private/${DOMAIN_NAME}.key"
+
 if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
     # -x509                 --> generates a certificate instead of a certificate request
     # -nodes                --> do not encryot the private key because needed by nginx
     # -days 365             --> valid for 1 year
-    # -subj "/CN=localhost" --> sets certificate subject: CN is the Common Name (domain name)
+    # -subj                 --> sets certificate subject: CN is the Common Name (domain name) 
+    #            --> the other flags are optional: C = country ; O = organization ; ST = state 
     # -newkey rsa:2048      --> creates a new 2048-bits (min recommended key size) RSA key pair
     # -keyout <>            --> path to save the private key
     # -out <>               --> path to save the certificate
     open ssl req -x509 -nodes -days 365 \
-    -subj "/CN=localhost" \
+    -subj "/C=FR/O=42/CN=${DOMAIN_NAME}" \
     -newkey rsa:2048 \
-    -keyout /etc/ssl/private/nginx-selfsigned.key \
-    -out /etc/ssl/certs/nginx-selfsigned.crt
+    -keyout "$KEY" \
+    -out "$CRT"
 fi
 
 wait-for-it.sh wordpress:9000 --timeout=30 --strict -- echo "Wordpress is ready"
+
+# to be able to refer to my DOMAIN_NAME envv in my nginx conf file 
+envsubst '${DOMAIN_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # -g 'daemon off;' --> overrides nginx default config to make sure that it runs in the foreground (and not background)
 # important bc otherwise it'll be daemonized (= go to the background) and the containers will exit immediately
