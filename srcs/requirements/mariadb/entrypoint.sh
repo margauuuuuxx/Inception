fc41e4af5460d6cb94c44485f1d668290c82f2fd
marlonco <marlonco@inception.myguest.virtualbox.org>
2025-05-30 10:15:11 +0200

    small fixes according to logs

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 # if the data directiry doesnt exist create it
-if [! -d /var/lib/mysql/mysql ]; then 
+if [ ! -d "/var/lib/mysql/mysql" ]; then 
     mysql_install_db --user=mysql --datadir=/var/lib/mysql #initializes the dir
 fi
 
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -8,4 +8,5 @@ wait-for-it.sh mariadb:3306 --timeout=30 --strict -- echo "MariaDB is ready"
 # use of the exec keyword --> replaces the current shell process with the proccess newly launched
 # by default in a container the first process started iks PID 1 (signa handling: SIGTERM, SIGINT, ...)
 # if no use of exec --> the script (= shell) remains PID 1 and the actual app becomes a child problem which is not good
-exec php-fpm7.4
+# -F = foreground mode 
+exec php-fpm7.4 -F
