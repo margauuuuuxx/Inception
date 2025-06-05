6e03f1647c24e83f69122121b858c1f55f0f4da8
marlonco <marlonco@inception.myguest.virtualbox.org>
2025-06-05 13:40:00 +0200

    letsgoooooo

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -47,7 +47,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
 	echo "🛠 WordPress website installation ..."
 	wp core install		--url=${DOMAIN_NAME} \
-						--title=${WP_TITLE} \
+						--title="${WP_TITLE}" \
 						--admin_user=${WP_ADMIN} \
 						--admin_password=${WP_ADMIN_PASSWORD} \
 						--admin_email=${WP_ADMIN_EMAIL} \
